e7a9e11a38709b4e94309359545e6db93ab346b3
ChewieCB <jackmccaffrey96@gmail.com>
2019-11-29 18:29:02 +0000

    Changed twilio credentials to test credentials to save money usage

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,20 +1,23 @@
 from twilio.rest import Client
+import json
 import yaml
+import requests
 
 # Import credentials from config file
 with open('config.yaml', 'r') as config_file:
-    config = yaml.load(config_file)
+    config = yaml.safe_load(config_file)
 
 twilio_credentials = config["twilio"]
 personal_numbers = config["personal numbers"]
 
 # Initialise the twilio client
+test_client = Client(twilio_credentials["TEST_SID"], twilio_credentials["TEST_TOKEN"])
 client = Client(twilio_credentials["SID"], twilio_credentials["TOKEN"])
 
 # Create a postbin for testing callbacks
-# create_bin = requests.post("https://postb.in/api/bin")
-# bin_id = create_bin.json()['binId']
-# test_bin = f"https://postb.in/{bin_id}"
+create_bin = requests.post("https://postb.in/api/bin")
+bin_id = json.loads(create_bin.text)['binId']
+test_bin = f"https://postb.in/api/bin/{bin_id}"
 
 
 def test_initialise_client():
@@ -23,7 +26,9 @@ def test_initialise_client():
     and that the auth credentials match the ones given.
     :return: None.
     """
+    assert test_client
     assert client
+    assert test_client.auth == (twilio_credentials["TEST_SID"], twilio_credentials["TEST_TOKEN"])
     assert client.auth == (twilio_credentials["SID"], twilio_credentials["TOKEN"])
 
 
@@ -33,13 +38,14 @@ def test_send_message():
     :return:
         """
     # TODO: fix status callback, getting a 404 when trying to retrieve the data from postbin
-    # client.messages.create(
-    #     to=credentials.personal_numbers[0],
-    #     from_=twilio_credentials["TRIAL_NUMBER"],
-    #     body="AUTOMATED PYTHON-TWILIO TEST, DO NOT RESPOND.",
-    #     status_callback=test_bin,
-    # )
-    # print(test_bin)
-    # callback = requests.get(test_bin)
-    # print(callback.text)
+    test_message = test_client.messages.create(
+        to="+15005550006",
+        from_=twilio_credentials["TEST_NUMBER"],
+        body="AUTOMATED PYTHON-TWILIO TEST, DO NOT RESPOND.",
+        status_callback=test_bin,
+    )
+    print(test_bin)
+    callback = requests.get(test_bin)
+    print(callback.text)
+    requests.delete(test_bin)
 
